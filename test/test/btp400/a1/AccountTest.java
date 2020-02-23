adf56f41b5b5ead231406463cca116c6dc39c304
Ruby <rabautista@myseneca.ca>
2020-02-23 17:41:27 -0500

    closing commit

--- test/test/btp400/a1/AccountTest.java
+++ test/test/btp400/a1/AccountTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
