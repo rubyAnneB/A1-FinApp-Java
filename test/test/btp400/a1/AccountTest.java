823748441f458cc8b45bd1caa51c79773d4de407
dderich <61340085+dderich@users.noreply.github.com>
2020-02-23 15:53:42 -0500

    Update AccountTest.java
    finished AccountTest JUnit, specs specify "create a suite of tests" NOT a case of JUnit tests

--- test/test/btp400/a1/AccountTest.java
+++ test/test/btp400/a1/AccountTest.java
@@ -4,90 +4,79 @@
  * @version 1.0
  * @since 2020-02-22
  * */
-
 package test.btp400.a1;
-import com.seneca.accounts.Account;
-import org.junit.Test;
+
+import com.seneca.accounts.*;
+//import com.seneca.business.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
-
-import java.text.NumberFormat;
-
+@RunWith(Suite.class)
+@SuiteClasses({})
 public class AccountTest {
+	@Test
+	public void testEmpty() {
+		Account ac = new Account();
+		System.out.println(ac);
+		assertEquals(ac, new Account("", "", 0.00));
+	}
 
-    @Test
-    public void neg_InitBalance(){
-        Account neg = new Account("John Smith", "2323",-323232);
-        assertEquals(BigDecimal.ZERO,neg.getAccountBalance());
-    }
-    @Test
-    public void nullName(){
-        Account noName = new Account(null, "2323",-323232);
-        assertEquals("",noName.getFullName());
+	@Test
+	public void neg_InitBalance() {
+		Account neg = new Account("John Smith", "2323", -323232);
+		assertEquals(BigDecimal.valueOf(0.00), neg.getAccountBalance());
+	}
 
-    }
-    @Test
-    public void getFirstName() {
-        Account fname= new Account("Fname Mname Lname","3213",1232.90909);
-        assertEquals("Fname",fname.getFirstName());
-    }
+	@Test
+	public void testNullAndNeg() {
+		Account nullAcc = new Account(null, null, -5);
+		System.out.println(nullAcc);
+		assertEquals(nullAcc, new Account("", "", 0.00));
+	}
 
-    @Test
-    public void getLastName() {
-        Account lname= new Account("Fname Lname","3213",12.909090);
-        assertEquals("Lname",lname.getLastName());
-    }
+	@Test
+	public void withdrawNegative() {
+		Account draw = new Account("John Martin", "3213", 1212.34);
+		assertFalse(draw.withdraw(-55));
+		System.out.println(draw);
+	}
 
-    @Test
-    public void noLastName(){
-        Account lname= new Account("Fname","3213",12);
-        assertEquals("Fname",lname.getLastName());
-    }
+	@Test
+	public void withdrawPositive() {
+		Account draw = new Account("Fname Mname Lname", "3213", 1212.34);
+		assertTrue(draw.withdraw(2));
+		System.out.println(draw);
 
-    @Test
-    public void manyNames(){
-        Account nameS= new Account("Fname Mname Lname","3213",12);
-        assertEquals("Mname Lname",nameS.getLastName());
+	}
 
-    }
+	@Test
+	public void negBalance() {
+		Account draw = new Account("Dan Harper", "5678", 1000);
+		assertFalse(draw.withdraw(1001.11));
+	}
 
-    @Test
-    public void withdrawPositive() {
-        Account draw= new Account("Fname Mname Lname","3213",1212.34);
-        assertTrue(draw.withdraw(2));
-        System.out.println(draw);
+	@Test
+	public void depositNeg() {
+		Account depNeg = new Account("John Smoth", "34242", 0);
+		BigDecimal bal = depNeg.getAccountBalance();
+		depNeg.deposit(-123.00);
+		assertEquals(bal, depNeg.getAccountBalance());
+	}
 
-    }
-
-    @Test
-    public void withdrawNegative(){
-        Account draw= new Account("Fname Mname Lname","3213",1212.34);
-        assertFalse(draw.withdraw(-12));
-        System.out.println(draw);
-    }
-
-    @Test
-    public void withdrawInsufficient(){
-        Account draw= new Account("Fname Mname Lname","3213",1212.34);
-        assertFalse(draw.withdraw(1212121212));
-        System.out.println(draw);
-    }
-
-    @Test
-    public void depositNeg() {
-        Account depNeg = new Account("John Smoth", "34242", 0);
-        depNeg.deposit(-123.00);
-        System.out.println(depNeg);
-    }
-
-    @Test
-    public void depositPos() {
-        Account depPos= new Account("John Smoth", "34242", 0);
-        depPos.deposit(123.00);
-        System.out.println(depPos);
-    }
-
-
+	@Test
+	public void finalBal() {
+		Account a = new Account("Daniel D", "TD01", 1000.00);
+		a.deposit(200.00);
+		a.withdraw(50.00);
+		assertEquals(BigDecimal.valueOf(1000.00 + 200.00 - 50.00), a.getAccountBalance());
+	}
 }
